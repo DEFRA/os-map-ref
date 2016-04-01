ee22009ee93c9e32ed17bdf166adcbbb763dad3e
Rob Nichols <git@nicholshayes.co.uk>
2016-04-01 17:04:20 +0100

    Changes tests over to rspec

---
+++ spec/os_map_ref_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'spec_helper'
+
+describe OsMapRef do
+
+  describe "::VERSION" do
+    it "should be a string" do
+      expect(described_class::VERSION).to be_a(String)
+    end
+  end
+end
+
