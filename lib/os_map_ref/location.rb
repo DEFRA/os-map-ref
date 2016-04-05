fcc016ec6544e4eb4f2b09ee1f92225923c8e9f1
Rob Nichols <robnichols.ea@gmail.com>
2016-04-05 10:21:21 +0100

    Correct error in grid skipped K instead of I Also tidy up prefix handling, and associated error catching

--- lib/os_map_ref/location.rb
+++ lib/os_map_ref/location.rb
@@ -62,11 +62,11 @@ module OsMapRef
     end
 
     def northing_from_map_reference
-      northing_easting_from_map_reference[0].to_s + map_reference_parts[2]
+      prefix_coordinates[0].to_s + map_reference_parts[2]
     end
 
     def easting_from_map_reference
-      northing_easting_from_map_reference[1].to_s + map_reference_parts[1]
+      prefix_coordinates[1].to_s + map_reference_parts[1]
     end
 
     # The parts should be a pair of letters then two sets of numbers
@@ -74,11 +74,24 @@ module OsMapRef
     def map_reference_parts
       @map_reference_parts ||= map_reference.split
     end
+    
+    def prefix
+      map_reference_parts[0]
+    end
 
-    # Returns array of [grid_northing, grid_easting] for the gird element
-    # matching the map reference start. So 'ST 58901 71053' will return [1, 3]
-    def northing_easting_from_map_reference
-      @northing_easting_from_map_reference ||= matrix.index map_reference_parts[0]
+    # Returns array of [grid_northing, grid_easting] for the grid element
+    # matching the map reference prefix. So 'ST 58901 71053' will return [1, 3]
+    # which are the coordinates of the prefix 'ST' in the grid.
+    def prefix_coordinates
+      @prefix_coordinates ||= get_prefix_coordinates
+    end
+    
+    def get_prefix_coordinates
+      matrix.index(prefix) || raise_prefix_error(prefix)
+    end
+    
+    def raise_prefix_error(prefix)
+      raise OsMapRef::Error, "Unable to process map reference #{@map_reference}: #{prefix} not found"
     end
 
     def matrix
@@ -94,12 +107,12 @@ module OsMapRef
         %w[HQ HR HS HT HU JQ JR JS JT JU],
         %w[HV HW HX HY HZ JV JW JX JY JZ],
         %w[NA NB NC ND NE OA OB OC OD OE],
-        %w[NF NG NH NJ NK OF OG OH OI OJ],
+        %w[NF NG NH NJ NK OF OG OH OJ OK],
         %w[NL NM NN NO NP OL OM ON OO OP],
         %w[NQ NR NS NT NU OQ OR OS OT OU],
         %w[NV NW NX NY NZ OV OW OX OY OZ],
         %w[SA SB SC SD SE TA TB TC TD TE],
-        %w[SF SG SH SJ SK TF TG TH TI TJ],
+        %w[SF SG SH SJ SK TF TG TH TJ TK],
         %w[SL SM SN SO SP TL TM TN TO TP],
         %w[SQ SR SS ST SU TQ TR TS TT TU],
         %w[SV SW SX SY SZ TV TW TX TY TZ]
